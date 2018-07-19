8ac357fe2b118a53de6d059511a105e8030eb488
Vitor Isaia <vitor@wplex.com.br>
2018-07-19 14:41:01 -0300

    first customizations

--- src/main/java/com/rawsanj/adminlte/web/controller/DashboardController.java
+++ src/main/java/com/rawsanj/adminlte/web/controller/DashboardController.java
@@ -3,7 +3,6 @@ package com.rawsanj.adminlte.web.controller;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Sanjay on 8/15/2016.
